db913019c294b749f754b962bc2b18cf9804a21e
Samantha Taylor <staylor@mort.is>
2020-06-16 14:38:34 -0400

    Clear button now removed the main tag

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 const bodyElement = document.querySelector('body')
 
-const buttonElement = document.createElement('button')
+const inputElement = document.createElement('input')
+bodyElement.append(inputElement)
+
+const removebuttonElement = document.createElement('button')
 let remove = document.createTextNode("Remove");
-buttonElement.appendChild(remove);  
+removebuttonElement.appendChild(remove);  
+bodyElement.append(removebuttonElement)
+
+removebuttonElement.addEventListener ("click", function(){
+    
+})
+
+const buttonElement = document.createElement('button')
+let clear = document.createTextNode("Clear");
+buttonElement.appendChild(clear);  
 bodyElement.append(buttonElement)
 
 const mainElement = document.createElement('main')
 bodyElement.append(mainElement)
+
+buttonElement.addEventListener ("click", function(){
+    mainElement.remove(mainElement)
+})
 
 const imgElement = document.createElement('img')
 imgElement.src = 'Picture/9eyyag09rax41.jpg';
@@ -18,5 +34,5 @@ const linkElement = document.createElement('a')
 let link = document.createTextNode("This is a link"); 
 linkElement.appendChild(link);  
 linkElement.classList.add('link')
-linkElement.href = "https://www.google.com";  
+linkElement.href = "https://www.bing.com";  
 mainElement.append(linkElement)
